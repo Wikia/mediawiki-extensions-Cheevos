c8718516ac914325f483771cb3ffce71314aa8c6
Alexia E. Smith <washuu@gmail.com>
2017-05-04 15:13:13 -0500

    Fix language string.

--- specials/SpecialWikiPoints.php
+++ specials/SpecialWikiPoints.php
@@ -70,7 +70,7 @@ class SpecialWikiPoints extends HydraCore\SpecialPage {
 
 		$pagination = HydraCore::generatePaginationHtml($total, $itemsPerPage, $start);
 
-		$this->output->setPageTitle(wfMessage('top_wiki_editors'.($isSitesMode ? '_sites' : '').($isMonthly ? '_monthly' : '')));
+		$this->output->setPageTitle(wfMessage('top_wiki_editors'.($isGlobal ? '_global' : '').($isSitesMode ? '_sites' : '').($isMonthly ? '_monthly' : '')));
 		$this->content = TemplateWikiPoints::getWikiPointsLinks()."<div>{$pagination}</div>".\Cheevos\Points\PointsDisplay::pointsBlockHtml(($isSitesMode || $isGlobal ? null : $dsSiteKey), null, $itemsPerPage, $start, $isSitesMode, $isMonthly)."<div>{$pagination}</div>";
 	}
 
