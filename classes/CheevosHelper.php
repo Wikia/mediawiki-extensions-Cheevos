17c731de9ff98f556f94851ccb974e26940b7497
Alexia E. Smith <washuu@gmail.com>
2020-02-10 15:45:26 -0600

    Just make that more inclusive.

--- classes/CheevosHelper.php
+++ classes/CheevosHelper.php
@@ -60,9 +60,8 @@ class CheevosHelper {
 				'last_incremented' => $stat['last_incremented'],
 			];
 			if (!isset($users[$stat['user_id']])) {
-
+				$users[$stat['user_id']] = Cheevos::getUserForServiceUserId($stat['user_id']);
 			}
-			$users[$stat['user_id']] = Cheevos::getUserForServiceUserId($stat['user_id']);
 			if (isset($stat['site_key']) && !empty($stat['site_key'])) {
 				$nice[$stat['site_key']][$users[$stat['user_id']]->getId()][$stat['stat']] = $_data;
 			} else {
