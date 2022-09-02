1c3a3bafa2add9c34ac72c994cb361877c7f59e5
mklucsarits <mattk@fandom.com>
2022-09-02 18:42:06 -0500

    debug

--- js/cheevos.pointscomp.js
+++ js/cheevos.pointscomp.js
@@ -7,9 +7,13 @@ var setupPointsCompDatepickers = function() {
 			dataInput: $('#start_time_datepicker').attr('data-input')
 		}
 	);
-	$("#start_time_datepicker").replaceWith(startDate.$element);
+	$('#start_time_datepicker').replaceWith(startDate.$element);
 	startDate.on('change', function() {
+		console.log('TEST startDate change');
+		console.log('TEST startDate value = ', startDate.getValue());
+		console.log('TEST date getTime = ', new Date(startDate.getValue()).getTime());
 		$('#'+startDate.dataInput).val(new Date(startDate.getValue()).getTime() / 1000);
+		console.log('TEST data input updated', $('#'+startDate.dataInput));
 	});
 }
 setupPointsCompDatepickers();
