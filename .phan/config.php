ed52a5ce50af77b13d5cd076389f215881c0ec60
Michał Chatłas <you@example.com>
2023-02-22 13:43:24 +0100

    Fixed further Phan problems

--- .phan/config.php
+++ .phan/config.php
@@ -3,7 +3,8 @@
 $cfg = require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config.php';
 
 $cfg['directory_list'] = [
-	'../../extensions/RedisCache'
+	'../../extensions/RedisCache',
+	'.'
 ];
 
 $cfg['exclude_analysis_directory_list'] = [
