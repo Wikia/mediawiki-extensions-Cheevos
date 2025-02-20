a6a429fceabda1aab75b603a702112375ff49e3a
Irina Balaban <ibalaban@fandom.com>
2025-02-20 10:58:12 +0100

    wip pr checks

--- .phan/config.php
+++ .phan/config.php
@@ -1,6 +1,52 @@
 <?php
 
+// TODO: stubs for internal code like WikiDomain
+
 $cfg = require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config.php';
+
+$mediawikiPath = getenv( 'MEDIAWIKI_PATH' ) ?? 'mediawiki/';
+$extensionsPath = getenv( 'EXTENSIONS_PATH' ) ?? 'mediawiki/extensions/';
+
+$cfg['autoload_internal_extension_signatures'] = [
+	'redis' => 'mediawiki/.phan/internal_stubs/redis.phan_php',
+];
+
+$mediawikiDirs = array_map(
+	fn ( $path ) => $mediawikiPath . $path,
+	[
+		'includes/',
+		'languages/',
+		'maintenance/',
+		'mw-config/',
+		'resources/',
+		'vendor/',
+	] );
+
+$extensionDependencyDirs = array_map(
+	fn ( $path ) => $extensionsPath . $path,
+	[
+		'RedisCache',
+		'Subscription',
+		'HydraCore',
+	],
+);
+
+$cfg['directory_list'] = array_merge(
+	$mediawikiDirs,
+	$extensionDependencyDirs,
+	[ $extensionsPath . 'Cheevos' ],
+);
+
+$cfg['exclude_analysis_directory_list'] = array_merge(
+	$mediawikiDirs,
+	$extensionDependencyDirs,
+	[
+		$extensionsPath . 'Cheevos/vendor/',
+		$extensionsPath . 'Cheevos/.phan/',
+	]
+);
+
+$cfg['scalar_implicit_cast'] = true;
 
 $cfg['suppress_issue_types'] = [
 	...$cfg['suppress_issue_types'],
